42047eb544c525c7bdeb0284f6b2560109f26362
FarawayVision <stmobo@gmail.com>
2019-08-24 16:45:35 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="waifustream_v2",
+    version="1.0.0",
+    url="https://github.com/stmobo/waifustream-v2.git",
+    author="FarawayVision <stmobo@gmail.com>",
+    description="Waifustream-v2 scraper and indexer",
+    packages=find_packages(),
+)
