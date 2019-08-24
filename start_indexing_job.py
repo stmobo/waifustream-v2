8cc9532a1767ed3eedf003f1720f379fe5dd7e03
FarawayVision <stmobo@gmail.com>
2019-08-24 17:40:43 -0400

    Extend test scripts to actually work

--- start_indexing_job.py
+++ start_indexing_job.py
@@ -7,11 +7,12 @@ from rq import Queue
 def main():
     redis_url = sys.argv[1]
     site = sys.argv[2]
+    character = sys.argv[3]
 
     redis = Redis.from_url(redis_url)
 
     q = Queue("scraper", connection=redis)
-    q.enqueue("indexer.scraper.worker.do_indexing_crawl", site)
+    q.enqueue("indexer.scraper.worker.do_indexing_crawl", site, character)
     print("Enqueued scraping job for " + site)
 
 
